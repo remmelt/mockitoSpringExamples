4f173d041074ac03f4214fc6411e95be95b8cda2
Remmelt Pit <remmelt@remmelt.com>
2013-09-04 21:09:41 +0200

    - Spring Test dependency - Wired the external DatabaseAccess bean manually - logback.xml to kill Spring logging

--- src/main/java/com/remmelt/example/configuration/ApplicationConfig.java
+++ src/main/java/com/remmelt/example/configuration/ApplicationConfig.java
@@ -1,9 +1,20 @@
 package com.remmelt.example.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.external.database.DatabaseAccess;
+import com.external.database.impl.DatabaseAccessImpl;
 
 @Configuration
 @ComponentScan("com.remmelt.example")
 public class ApplicationConfig {
+
+	// Since the "external" class DatabaseAccess is not marked as @Service or @Repository we're going to @Bean it explicitly
+	@Bean
+	public DatabaseAccess databaseAccess() {
+		return new DatabaseAccessImpl();
+	}
+
 }
